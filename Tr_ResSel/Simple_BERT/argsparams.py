6aa29f7ec52ce2a30a9ce2008024fcd1665795b2
Elahe-rb <rabbani.elahe@gmail.com>
2021-11-16 13:43:33 +0330

    train dev and test seperated

--- Tr_ResSel/Simple_BERT/argsparams.py
+++ Tr_ResSel/Simple_BERT/argsparams.py
@@ -6,7 +6,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 #Dataset path.
 FT_data={
-    'ubuntu': 'ubuntu_data/', #for colab: '../../../../data/BERT/ubuntu_data/ubuntu_dataset_1M.pkl',   #'ubuntu': 'ubuntu_data/ubuntu_dataset_1M.pkl'
+    'ubuntu': '../../../../data/BERT/ubuntu_data/', #for colab: '../../../../data/BERT/ubuntu_data/ubuntu_dataset_1M.pkl',   #for mylap 'ubuntu': 'ubuntu_data/ubuntu_dataset_1M.pkl'
     'douban': 'douban_data/douban_dataset_1M.pkl',
     'e_commerce': 'e_commerce_data/e_commerce_dataset_1M.pkl'
 }
